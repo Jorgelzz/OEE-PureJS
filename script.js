97b565504329a53a7a51867c043d791940fed7f9
Jorge Luiz <jorgeluiz2027@hotmail.com>
2025-01-30 12:44:52 -0300

    Initial Commit

---
+++ script.js
@@ -0,0 +1,10 @@
+// Pegar valores do DOM corretamente, estar dentro de um evento
+
+document.addEventListener('DOMContentLoaded', Calculate =>{
+    
+    let availabilityMachine = document.getElementById('availability');
+    let productivityMachine = document.getElementById('productivity');
+    let qualityMachine = document.getElementById('quality');
+
+})
+
