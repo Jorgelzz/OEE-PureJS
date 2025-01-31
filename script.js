fa25c468259f2f3d488c9d46ae85490931190cd8
Jorge Luiz <jorgeluiz2027@hotmail.com>
2025-01-31 10:38:44 -0300

    Add some logics, working with createElement and getElements

--- script.js
+++ script.js
@@ -1,10 +1,33 @@
-// Pegar valores do DOM corretamente, estar dentro de um evento
 
-document.addEventListener('DOMContentLoaded', Calculate =>{
+
+const gerarOEE = document.getElementById('generator')
+
+gerarOEE.addEventListener('click', () => {
+
+    availability =  document.getElementById('availability').innerText = Number(Math.random()).toFixed(2)
+    productivity = document.getElementById('productivity').innerText = Number(Math.random()).toFixed(2)
+    quality = document.getElementById('quality').innerText = Number(Math.random()).toFixed(2)
+    OEE = parseFloat((availability * productivity * quality) * 100).toFixed(2)
+
     
-    let availabilityMachine = document.getElementById('availability');
-    let productivityMachine = document.getElementById('productivity');
-    let qualityMachine = document.getElementById('quality');
-
+    
 })
+
+const addOEE = document.getElementById('addOEE')
+const table = document.getElementById('tabelaOEE')
+
+addOEE.addEventListener('click', () => {
+    
+    const novaLinha = document.createElement('tr')
+
+    novaLinha.innerHTML = `
+        <td>${new Date().toLocaleDateString()}</td> <!-- Data atual -->
+        <td>${availability * 100}%</td> <!-- Disponibilidade aleatória -->
+        <td>${productivity * 100}%</td> <!-- Produtividade aleatória -->
+        <td>${quality * 100}%</td> <!-- Qualidade aleatória -->
+        <td>${OEE}%</td> <!-- OEE aleatório -->
+    `;
+    table.append(novaLinha)
+    
+} )
 
