d128484cb7ba1d1edf4033cb43a479c2609f5555
Jorge Luiz <136937143+Jorgelzz@users.noreply.github.com>
2025-02-03 21:31:35 -0300

    Update script.js
    Removing blank spaces

--- script.js
+++ script.js
@@ -1,25 +1,15 @@
-
-
 const gerarOEE = document.getElementById('generator')
-
 gerarOEE.addEventListener('click', () => {
 
     availability =  document.getElementById('availability').innerText = Number(Math.random()).toFixed(2)
     productivity = document.getElementById('productivity').innerText = Number(Math.random()).toFixed(2)
     quality = document.getElementById('quality').innerText = Number(Math.random()).toFixed(2)
     OEE = parseFloat((availability * productivity * quality) * 100).toFixed(2)
-
-    
-    
 })
-
 const addOEE = document.getElementById('addOEE')
 const table = document.getElementById('tabelaOEE')
-
 addOEE.addEventListener('click', () => {
-    
     const novaLinha = document.createElement('tr')
-
     novaLinha.innerHTML = `
         <td>${new Date().toLocaleDateString()}</td> <!-- Data atual -->
         <td>${availability * 100}%</td> <!-- Disponibilidade aleatória -->
@@ -28,6 +18,5 @@ addOEE.addEventListener('click', () => {
         <td>${OEE}%</td> <!-- OEE aleatório -->
     `;
     table.append(novaLinha)
-    
 } )
 
